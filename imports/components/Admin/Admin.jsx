b1f85cf4556a7a10e8867ba87dab3bbc60e44091
bartwr <mail@bartroorda.nl>
2018-01-20 23:42:48 +0100

    Added basic Admin panel

---
+++ imports/components/Admin/Admin.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Admin extends Component {
+ 
+  render() {
+    return (
+      <div style={s.base} className="page-module">
+        <div className="page-inner">
+
+          <h2>DeepDive Admin</h2>
+
+          
+
+        </div>
+      </div>
+    );
+  }
+}
+
+var s = {
+  base: {
+  },
+}
