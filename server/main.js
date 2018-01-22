4f0ac673fe2198fbbff45b90a7ba2d1774b0a78c
bartwr <mail@bartroorda.nl>
2018-01-22 14:28:53 +0100

    Made it possible to send mail via Mailgun

--- server/main.js
+++ server/main.js
@@ -8,4 +8,12 @@ import '/imports/server/publish.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Future = Npm.require('fibers/future');
+
+  var username = "postmaster@mg.deepdive.training";
+  var password = "7d8a2e1969d4928d24b87290eee332ee";
+  var server = "smtp.mailgun.org";
+  var port = "587";
+
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + encodeURIComponent(server) + ':' + port;  
 });
