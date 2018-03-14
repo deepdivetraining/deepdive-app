bf7309fc728757d28939b84a5ac42f2df41b3748
bartwr <mail@bartroorda.nl>
2018-03-15 00:59:54 +0100

    Select right web3 provider

--- public/sec-101/vote/vote.js
+++ public/sec-101/vote/vote.js
@@ -1,9 +1,7 @@
 /**
  * Source: https://gist.githubusercontent.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4/raw/e7e7d4e063c5bdd4fb6bc17d7b75370de42d48ee/index.js
  */
-if(!web3) {
-  web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/4V5xU3MkRN6Lw3b9pMGh"));
-}
+web3 = new Web3(Web3.givenProvider || Web3.currentProvider || new Web3.providers.HttpProvider("https://ropsten.infura.io/4V5xU3MkRN6Lw3b9pMGh"));
 abi=[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidateNames","type":"bytes32[]"},{"name":"secretkey","type":"bytes32"}],"name":"initCandidates","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"secretkey","type":"bytes32"}],"name":"resetVotes","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
 
 DeepDiveVoting = web3.eth.contract(abi);
@@ -14,7 +12,7 @@ candidates = {"good": "candidate-1", "bad": "candidate-2", "ugly": "candidate-3"
 
 function voteForCandidate() {
   candidateName = $("#candidate").val();
-  contractInstance.voteForCandidate(candidateName, {from: web3.eth.defaultAccount, value: 0, gas: 2000}, function(err, res) {
+  contractInstance.voteForCandidate(candidateName, {from: web3.eth.defaultAccount}, function(err, res) {
     let div_id = candidates[candidateName];
     $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
   });
